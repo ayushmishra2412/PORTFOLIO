da1ad6983b4c7bc1bfe06e74e68d4a5ca23092a2
ayushmishra2412 <114082688+ayushmishra2412@users.noreply.github.com>
2024-04-03 23:34:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// var typed = new Typed(".text",{
+//     Strings:["Frontend Development","App Development","UI/UX Design"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// });
